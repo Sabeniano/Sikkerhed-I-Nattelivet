b17d043f2f9664d03c121dfedfd4a7a7d4de5623
Jason Sabeniano <35453938+Sabeniano@users.noreply.github.com>
2023-04-02 01:18:50 +0200

    Update networking.py

--- ESP-32/networking.py
+++ ESP-32/networking.py
@@ -2,28 +2,22 @@ import network
 from time import sleep
 from credentials import credentials
 
-# WiFi connection information
+# WiFi information
 WIFI_SSID = credentials["ssid"]
 WIFI_PASSWORD = credentials["password"]
 
 def connect_to_wifi():
-    # Create a WLAN interface
     wlan = network.WLAN(network.STA_IF)
     
-    #Fixer WiFi OS fejl!
     if (wlan.isconnected()):
         wlan.disconnect()
     
-    # Activate the WLAN interface
     wlan.active(True)
     
-    # Connect to the network
     wlan.connect(WIFI_SSID, WIFI_PASSWORD)
     
-    # Wait for the connection to be established
     while not wlan.isconnected():
         sleep(1)
     print("Connected to WiFi network.")
-    # Print the network configuration
     print("Network configuration:", wlan.ifconfig())
     return wlan
