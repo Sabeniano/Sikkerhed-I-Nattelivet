685a6c823e042b9942cc5fde98c2e1fa1119e7a5
Jason Sabeniano <35453938+Sabeniano@users.noreply.github.com>
2023-04-02 01:19:56 +0200

    Update bytes_to_voltage.py

--- ESP-32/bytes_to_voltage.py
+++ ESP-32/bytes_to_voltage.py
@@ -1,22 +1,9 @@
 import struct
 
-    ##https://www.w3schools.com/python/gloss_python_bitwise_operators.asp
-    # unpack() # tager en bytestring og konvertere det til en tuple af værdier baseret på '>h'
-    # >h       # 'h' specificerer, at dataen er en 2-byte (16-bit) signed integer.
-    # [0]      # [0] i slutningen af struct.unpack() metoden tilgår den første (og eneste) værdi i tuplen. 
-    # >>       # [0] tuple værdien "shiftes" 2 bits til højre med >> operator for at opnå den 10-bit ADC-værdi.
-    #
-    # De to bytes som bliver send til data
-    # udpakkes som en stor-endian 16-bit signed integer
-    # og derefter konverteres til en 10-bit signed integer
-    # ved at shiftet værdien 2 bits til højre.
-
-#383
 #findes ved at tage en måling på sit batteri, kaldet U
 #U*1023/adc_val = u_num
 u_num = 15.86271255060729
 def calculate_voltage(byte_from_adc):
-    ##https://www.w3schools.com/python/gloss_python_bitwise_operators.asp
     value = struct.unpack('>h', byte_from_adc)[0] >> 2
 
     voltage = (value * u_num) / 1023.0
@@ -26,5 +13,4 @@ def calculate_voltage(byte_from_adc):
     print("ADC Value: {}".format(value))
     print("Voltage: {:.2f} V".format(voltage))
     print("Battery Percentage: {:.2f}%".format(battery_percentage))
-
     return battery_percentage
